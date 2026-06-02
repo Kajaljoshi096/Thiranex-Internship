c79a9919db14eac35e2895f6fbc918106b2c0450
Kajaljoshi096 <theonlyoneguy123@gmail.com>
2026-06-02 18:14:04 +0530

    app.py

--- Password-Strength-Analyzer/app.py
+++ Password-Strength-Analyzer/app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template, request
+from password_checker import check_password
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+
+    if request.method == "POST":
+        password = request.form["password"]
+        score, strength, feedback = check_password(password)
+
+        result = {
+            "score": score,
+            "strength": strength,
+            "feedback": feedback
+        }
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
